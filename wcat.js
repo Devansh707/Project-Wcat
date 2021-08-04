eb3395a18bffda4f0bb2daf652a33e64054497c2
Devansh707 <devanshsrivastava707@gmail.com>
2021-08-04 17:58:50 +0530

    completed

--- wcat.js
+++ wcat.js
@@ -38,15 +38,14 @@ let content = "";
 console.log("New length of Files Array = " , newFilesArr.length );
 console.log("\n");
 for(let i=0; i<newFilesArr.length; i++) {
-    content += fs.readFileSync(newFilesArr[i]) + "\n";
+    content += fs.readFileSync(newFilesArr[i]) + "\r\n";
 }
-// console.log(content);
-// 3. -s implementation 
+console.log(content);
+// *****************************3. -s implementation ***************
 // -s checks 
-let textfile = [];
-let isS_Present = optionsArr.includes("-s");
-if(isS_Present) {
-    let contentArr = content.split("\r\n");
+let contentArr = content.split("\r\n");
+let ifS_Present = optionsArr.includes("-s");
+if(ifS_Present) {
     for(let i = 1; i < contentArr.length; i++) {
         if(contentArr[i] == "" && contentArr[i - 1] == ""){
             contentArr[i] = null;
@@ -61,17 +60,53 @@ if(isS_Present) {
         }
     }
     contentArr = tempArr;
-    textfile = contentArr;
     console.log(contentArr.join("\n"));
 }
 
-// -n check for
-let counter = 0;
-for(let i = 0; i < textfile.length; i++) {
-    if( textfile[i] == "" ){
-        continue;
-    }else{
-    console.log( (++counter) + ". " + textfile[i] + "\n" );
-        
+// **********************check for -n and -b ************
+let finalOption = "";
+let indexOfN = optionsArr.indexOf("-n");
+let indexOfB = optionsArr.indexOf("-b");
+
+if( indexOfN > -1 && indexOfB > -1) {
+    // Both are present
+    if(indexOfN < indexOfB) {
+        // Index of N is smaller than index of B so gets first priority
+        finalOption = "-n";
+    } else if (indexOfB < indexOfN){
+        // Index of B is smaller than index of N so gets first priority
+        finalOption = "-b";         
+    }
+} else {
+    // Only one of the two is present 
+    if(indexOfN > -1){
+        finalOption = "-n";
+
+    } else if (indexOfB > -1){
+        finalOption = "-b";         
+
     }
 }
+console.log("final option = ", finalOption);
+if( finalOption == "-n" ){
+    modifyContentByN(contentArr)
+    
+} else if ( finalOption == "-b" ) {
+    modifyContentByB(contentArr);
+}
+function modifyContentByN(contentArr){
+    for(let i = 0; i < contentArr.length; i++){
+        contentArr[i] = (i + 1) + ". " + contentArr[i];
+    }
+}
+
+function modifyContentByB(contentArr){
+    let count = 1;
+    for(let i = 0 ; i < contentArr.length; i++) {
+        if( contentArr[i] != "" ){
+            contentArr[i] = count + ". " + contentArr[i];
+            count++;
+        }
+    }
+}
+console.log(contentArr.join("\r\n"));
